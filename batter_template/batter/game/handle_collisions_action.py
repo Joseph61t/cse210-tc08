e75b8328c4dc1679cfbc0108b35823155ed3f293
JeremyCBusch <64992102+JeremyCBusch@users.noreply.github.com>
2021-06-14 17:54:53 -0600

    if statements for collisions
    hopefully this makes sense

--- batter_template/batter/game/handle_collisions_action.py
+++ batter_template/batter/game/handle_collisions_action.py
@@ -1,6 +1,8 @@
+from batter_template.batter.game import point
 import random
 from game import constants
 from game.action import Action
+from game.point import Point
 
 class HandleCollisionsAction(Action):
     """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
@@ -15,11 +17,27 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        marquee = cast["marquee"][0] # there's only one
-        robot = cast["robot"][0] # there's only one
-        artifacts = cast["artifact"]
-        marquee.set_text("")
-        for artifact in artifacts:
-            if robot.get_position().equals(artifact.get_position()):
-                description = artifact.get_description()
-                marquee.set_text(description) 
+        ball = cast["ball"][0] # there's only one
+        brick = cast["brick"][0] # there's only one
+        # artifacts = cast["artifact"]
+        brick.set_text("")
+        #for artifact in artifacts:
+
+
+        # the if statements the cover when a ball hits a brick
+        # covers if the ball hits a brick from bellow
+        if ball.get_y(ball.get_position()) - 1 == brick.get_y(ball.get_position() and ball.get_x(ball.get_position()) == brick.get_x(ball.get_position()):
+            point = Point(ball.set_x(), (ball.set_y() * -1))
+            ball.set_velocity(point)
+        # covers if the ball hits a brick from above
+        if ball.get_y(ball.get_position()) + 1 == brick.get_y(ball.get_position() and ball.get_x(ball.get_position()) == brick.get_x(ball.get_position()):
+            point = Point(ball.set_x(), (ball.set_y() * -1))
+            ball.set_velocity(point)
+        # covers if the ball hits a brick from the left
+        if ball.get_x(ball.get_position()) - 1 == brick.get_x(ball.get_position() and ball.get_y(ball.get_position()) == brick.get_y(ball.get_position()):
+            point = Point((ball.set_x() * -1), ball.set_y())
+            ball.set_velocity(point) 
+        # covers if the ball hits a brick from the right
+        if ball.get_x(ball.get_position()) + 1 == brick.get_x(ball.get_position() and ball.get_y(ball.get_position()) == brick.get_y(ball.get_position()):
+            point = Point((ball.set_x() * -1), ball.set_y())
+            ball.set_velocity(point)
